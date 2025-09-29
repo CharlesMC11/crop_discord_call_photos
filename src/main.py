f7de98072ff09d84073f6bf7f2c914653d3d669e
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2025-09-29 14:25:35 -0700

    Add image file extensions

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ MIN_IMG_AREA = 50_000
 MAX_IMG_AREA = MAX_CROPPED_HEIGHT * MAX_CROPPED_WIDTH
 
 NEW_SUFFIX = "_new"
-IMAGE_EXTENSIONS = {"png", "jpg", "jpeg", "tif", "tiff"}
+IMAGE_EXTENSIONS = {"heif", "jpeg", "jpg", "png", "tif", "tiff"}
 
 
 def crop(image: Path) -> Path:
